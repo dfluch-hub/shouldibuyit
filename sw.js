7f948cfce0cbf0ac9475ee94b748f8b5b3610df1
dfluch-hub <d.fluch@gmx.at>
2026-09-13 12:55:25 +0200

    Add offline service worker

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE='should-i-buy-it-v1';
+const CORE=['./','./index.html','./manifest.json','./icon.png'];
+self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).catch(()=>{}));self.skipWaiting();});
+self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE).map(key=>caches.delete(key)))));self.clients.claim();});
+self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy)).catch(()=>{});return response;}).catch(()=>caches.match('./index.html'))));});
